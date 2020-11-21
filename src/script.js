6d6892dab9b939c08f7506531b02783a2bc9f3f7
Nazira Rezky Afifah <nazira1402@mhs.unsyiah.ac.id>
2020-11-21 20:49:19 +0700

    memperbarui css

--- src/script.js
+++ src/script.js
@@ -69,7 +69,7 @@ function checkWin(board, player) {
 function gameOver(gameWon){
   for (let index of winCombos[gameWon.index]) {
     document.getElementById(index).style.backgroundColor = 
-      gameWon.player === huPlayer ? "blue" : "red";
+      gameWon.player === huPlayer ? "blue" : "#ff6666";
   }
   for (let i=0; i < cells.length; i++) {
     cells[i].removeEventListener('click', turnClick, false);
@@ -92,7 +92,7 @@ function bestSpot(){
 function checkTie() {
   if (emptySquares().length === 0){
     for (cell of cells) {
-      cell.style.backgroundColor = "green";
+      cell.style.backgroundColor = "#79ff4d";
       cell.removeEventListener('click',turnClick, false);
     }
     declareWinner("Tie game");
